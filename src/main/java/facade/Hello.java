a5e7d63ac526125caf18180c5e4d8976453575bb
Marcus Hindsbøl <cph-1054@cphbusiness.dk>
2023-02-03 09:19:18 +0100

    CVirk nu pls

--- src/main/java/facade/Hello.java
+++ src/main/java/facade/Hello.java
@@ -1,61 +1,113 @@
 package facade;
 
 public class Hello {
-
     public String sayHello(Object o) {
-        String name;
-        String[] nameArray;
+        String name = "";
 
         if (o instanceof String[]) {
-            nameArray = (String[]) o;
-            boolean checkForUpper = false;
+            String[] nameArray = (String[]) o;
+            boolean checkForComma = false;
+            for(String s: nameArray){
+                if(s.contains(",")){
+                    checkForComma = true;
+
+                }
+            }
+
+            if(checkForComma == true){
+                String stringToReturn = "";
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("Hello, ");
+                for(String s: nameArray){
+                    System.out.println(s + " THIS IS S");
+                    if(s.contains(",") == true){
+                        System.out.println("IM INSIDE");
+                        String[] strings =  s.split(",");
+
+                        if(s == nameArray[nameArray.length-1] ){
+                            stringBuilder.append(strings[0] + ", ");
+                            stringBuilder.append( "and " + strings[1] + ".");
+                        }
+
+
+                    }
+                    if(!s.contains(",") && s != nameArray[nameArray.length-1]){
+                        stringBuilder.append(s + ", ");
+                    }
+                    if(!s.contains(",") && s == nameArray[nameArray.length-1]){
+                        stringBuilder.append("and " + s + ".");
+                    }
+                    stringToReturn = stringBuilder.toString();
+                }
+                return stringToReturn;
+            }
+
+
+            boolean checkforUCname = false;
+            for (String s : nameArray) {
+                if (s.chars().allMatch(Character::isUpperCase) == true) {
+                    checkforUCname = true;
+                }
+            }
+            if (checkforUCname == true) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("Hello, ");
+                for (String s : nameArray) {
+                    if(s.chars().allMatch(Character::isUpperCase) == false && s == nameArray[nameArray.length-1]){
+                        stringBuilder.append(s + ". ");
+                    }
+                    if (s.chars().allMatch(Character::isUpperCase) == false && s != nameArray[nameArray.length-1]) {
+                        stringBuilder.append(s + " and ");
+                    }
+                }
+                stringBuilder.append("AND HELLO ");
+                for (String s : nameArray) {
+                    if (s.chars().allMatch(Character::isUpperCase) == true) {
+                        stringBuilder.append(s.toUpperCase() + "!");
+                    }
+                }
+                String stringToReturn = stringBuilder.toString();
+                return stringToReturn;
+
+
+
+            }
+
+
             if (nameArray.length <= 2) {
-            return "Hello, " + nameArray[0] + " and " + nameArray[1];
-        }
-
-            StringBuilder stringBuilder1 = new StringBuilder();
-            stringBuilder1.append("Hello, ");
-            for (String s: nameArray) {
-                if (s.chars().anyMatch(Character::isUpperCase)) {
-                    stringBuilder1.append(" AND HELLO ").append(s);
+                return "Hello, " + nameArray[0] + " and " + nameArray[1] + ".";
+            }
+            if (nameArray.length >= 3) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append("Hello, ");
+                for (String s : nameArray) {
+                    if (s == nameArray[nameArray.length - 1]) {
+                        stringBuilder.append("and " + s + ".");
+                    }
+                    if (s != nameArray[nameArray.length - 1]) {
+                        stringBuilder.append(s + ", ");
+                    }
                 }
-                if (s == nameArray[nameArray.length - 1]) {
-                    stringBuilder1.append("and ").append(s).append(".");
-                }
-                if (s != nameArray[nameArray.length - 1]) {
-                    stringBuilder1.append(s).append(", ");
-                }
-                String returnString = stringBuilder1.toString();
-                return returnString;
+                String stringToReturn = stringBuilder.toString();
+                return stringToReturn;
             }
 
-
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("Hello, ");
-            for (String s: nameArray) {
-                if (s == nameArray[nameArray.length - 1]) {
-                    stringBuilder.append("and ").append(s);
-                }
-                if (s != nameArray[nameArray.length - 1]) {
-                    stringBuilder.append(s).append(", ");
-                }
-            }
-            String stringToReturn = stringBuilder.toString();
-            return stringToReturn;
         }
-
 
         if (o instanceof String) {
             name = (String) o;
-            if (o == null) {
-                return "Hello, my friend";
+            if (name == null) {
+                return "Hello, my friend.";
             }
-            if (name.chars().allMatch(Character::isUpperCase)) {
+            if (name.chars().allMatch(Character::isUpperCase) == true) {
                 return "HELLO, " + name.toUpperCase() + "!";
             }
-            return "Hello, " + name;
-        }
-        return "Hello my friend";
-    }
+            return "Hello, " + name + ".";
 
+
+
+
+        }
+        return "Hello, my friend.";
+    }
 }
--- src/test/java/facade/HelloTest.java
+++ src/test/java/facade/HelloTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HelloTest {
+public class HelloTest {
 
     String name = "Helge";
     Hello hello;
@@ -22,47 +22,59 @@ class HelloTest {
 
     @Test
     public void testHello(){
-        System.out.println("Method to say hello");
-        String name = "Helge";
+        System.out.println("method to say hello");
         Hello hello = new Hello();
         String actual = hello.sayHello(name);
-        String expected = "Hello, Helge";
-        assertEquals(actual, expected);
+        String expected = "Hello, " + name + ".";
+        assertEquals(expected,actual);
     }
-
     @Test
     public void testHelloNull(){
-        System.out.println("Method to check for null");
+        System.out.println("method to check for null");
         String actual = hello.sayHello(null);
-        String expected = "Hello my friend";
-        assertEquals(actual, expected);
+        String expected = "Hello, my friend.";
+        assertEquals(expected,actual);
     }
-
     @Test
     public void testHelloShout(){
-        System.out.println("Method to check for shout");
+        System.out.println("method to check for shout");
         String actual = hello.sayHello("JERRY");
         String expected = "HELLO, JERRY!";
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
-/*
     @Test
     public void testHelloArray(){
-        System.out.println("Method to check for array");
+        System.out.println("method to check for helloarray");
         String[] nameArray = {"Jill", "Jane"};
         String actual = hello.sayHello(nameArray);
-        String expected = "Hello, Jill and Jane";
-        assertEquals(actual, expected);
+        String expected = "Hello, Jill and Jane.";
+        assertEquals(expected,actual);
     }
-
     @Test
     public void testHelloArrayArbitrary(){
-        System.out.println("Method to check for arbitrary names");
-        String[] nameArray = {"Jill", "Jane", "Charlotte"};
+        System.out.println("method to check for arbritary names");
+        String[] nameArray = {"Amy", "Brian", "Charlotte"};
         String actual = hello.sayHello(nameArray);
-        String expected = "Hello, Jill, Jane, and Charlotte";
-        assertEquals(actual, expected);
+        String expected = "Hello, Amy, Brian, and Charlotte.";
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testHelloArrayMixed(){
+        System.out.println("method to check for arbritary AND MIXED names");
+        String[] nameArray = {"Amy", "BRIAN", "Charlotte"};
+        String actual = hello.sayHello(nameArray);
+        String expected = "Hello, Amy and Charlotte. AND HELLO BRIAN!";
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testHelloCommaSplit(){
+        System.out.println("method to check for comma split");
+        String[] nameArray = {"Bob", "Charlie,Dianne"};
+        String actual = hello.sayHello(nameArray);
+        String expected = "Hello, Bob, Charlie, and Dianne.";
+        assertEquals(expected,actual);
     }
 
-    */
+
+
 }
